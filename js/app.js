fe3e31448af023dd93b97a5b21dc04f9c2842c00
AjayDumaraliya19 <dumaraliyaajay1912@gmail.com>
2023-04-27 17:27:37 +0530

    timer

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.food-slider').slick({
         autoplay: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow: ".prev-btn",
         nextArrow: ".next-btn",
-        responsive: [
-            {
+        responsive: [{
                 breakpoint: 993,
                 settings: {
                     slidesToShow: 2,
@@ -21,7 +20,43 @@ $(document).ready(function(){
         ]
     });
 
-    $('.nav-tigger').click(function(){
+    $('.nav-tigger').click(function () {
         $('.site-content-wrapper').toggleClass('scaled');
     })
 });
+
+var tim;
+
+var compareDate = new Date();
+compareDate.setDate(compareDate.getDate() + 3); //just for this demo today + 7 days
+
+tim = setInterval(function() {
+  timeBetweenDates(compareDate);
+}, 1000);
+
+function timeBetweenDates(toDate) {
+  var dateEntered = toDate;
+  var now = new Date();
+  var difference = dateEntered.getTime() - now.getTime();
+
+  if (difference <= 0) {
+    // Timer done
+    clearInterval(tim);
+  
+  } else {
+    
+    var seconds = Math.floor(difference / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+
+    hours %= 24;
+    minutes %= 60;
+    seconds %= 60;
+
+    $("#days").text(days);
+    $("#hours").text(hours);
+    $("#minutes").text(minutes);
+    $("#seconds").text(seconds);
+  }
+}
